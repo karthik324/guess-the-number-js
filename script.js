982359b64dd8bbe5c6db3d2a59ab7bb4668baa84
karthik324 <karthikdileep003@gmail.com>
2023-01-19 02:14:17 +0530

    game logic building
    score and number guessing DOM

--- script.js
+++ script.js
@@ -6,7 +6,33 @@ document.querySelector('.guess').value = 18;
 console.log(document.querySelector('.guess').value);
 */
 
-document.querySelector('.check').addEventListener('click', function (event) {
-    console.log(document.querySelector('.guess').value);
-    console.log(event);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+        document.querySelector('.message').textContent = 'You lost';
+        document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+        document.querySelector('.message').textContent = '📉 Too Low';
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+          document.querySelector('.message').textContent = 'You lost';
+          document.querySelector('.score').textContent = 0;
+      }
+  }
 });
